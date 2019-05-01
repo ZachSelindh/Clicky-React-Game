976d874fe91165e831725b4e29dc5c506ed5236e
Zach Selindh <zachselindh@gmail.com>
2019-05-01 18:11:34 -0500

    Added production build for deployment.

---
+++ build/precache-manifest.24bc5ddc01c3cf7e560be7c41a64a1f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d944fc971979ff002fb1bdd2f2e209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a287deab440fb5e7577",
+    "url": "/static/css/main.7a92ec5c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7a287deab440fb5e7577",
+    "url": "/static/js/main.f2538ed0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

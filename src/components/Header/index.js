ec63f43cf701484388057c12fa8c85725deade15
Zach Selindh <zachselindh@gmail.com>
2019-05-01 17:46:42 -0500

    Added additional styling and font.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,10 +4,19 @@ import "./style.css";
 function Header() {
   return (
     <div className="page-header">
-      <h1>Avengers, Assemble!</h1>
-      <p>
-        Click an Avenger to add them to your team. But don't click them twice!
-      </p>
+      <div className="row">
+        <div className="col-sm-12 col-md-8">
+          <h1>Avengers, Assemble!</h1>
+          <p>
+            Click an Avenger to add them to your team. But don't click them
+            twice!
+          </p>
+        </div>
+        <div id="scoreBoard" className="col-sm-12 col-md-4">
+          <h2 id="currentScore">Current Score: </h2>
+          <h2 id="highestScore">Highest Score: </h2>
+        </div>
+      </div>
     </div>
   );
 }

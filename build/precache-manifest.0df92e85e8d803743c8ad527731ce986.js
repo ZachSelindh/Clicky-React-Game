95c2a8e1fa2d53b8f397ecfef719f6832b822110
Zach Selindh <zachselindh@gmail.com>
2019-05-04 22:00:03 -0500

    Deployed to heroku

---
+++ build/precache-manifest.0df92e85e8d803743c8ad527731ce986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713479ac2d06cbaeb4018ee8ae9d84a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7717f188c26ff8d52546",
+    "url": "/static/css/main.409a0b35.chunk.css"
+  },
+  {
+    "revision": "3f886cee276095e1d4f3",
+    "url": "/static/js/2.0d0506dc.chunk.js"
+  },
+  {
+    "revision": "7717f188c26ff8d52546",
+    "url": "/static/js/main.e76e3c56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
